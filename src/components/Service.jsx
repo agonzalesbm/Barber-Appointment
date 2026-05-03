af1016d95c078612e7203d4ad79ef948c64526c1
agonzalesbm <andres.gonzales.dev@gmail.com>
2026-05-02 21:59:33 -0400

    [@agonzalesbm] README update, service section implement

---
+++ src/components/Service.jsx
@@ -0,0 +1,44 @@
+import "../index.css";
+import ServiceCard from "./ServiceCard";
+
+const Service = () => {
+  return (
+    <div style={{
+      display: "flex",
+      flexDirection: "column"
+    }}>
+      <div style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        gap: 24
+      }}>
+        <p style={{
+          magin: 0,
+          marginBottom: "5vh",
+          fontSize: "1.4rem",
+          lineHeight: 0
+        }}>
+          SERVICE
+        </p>
+        <p className="title-lines">
+          SCULPTING<br/>
+          STYLE, SHAPING<br/>
+          CHARACTER
+        </p>
+      </div>
+      <div id="service-card-grid">
+        <ServiceCard number={1} title={"Title"} paragraph={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Id reprehenderit voluptas ipsum incidunt aut soluta, tempora repellat fugit doloremque dolor!"}/>
+        <ServiceCard number={2} title={"Title"} paragraph={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Id reprehenderit voluptas ipsum incidunt aut soluta, tempora repellat fugit doloremque dolor!"}/>
+        <ServiceCard style={{
+          justifySelf: "right"
+        }} number={3} title={"Title"} paragraph={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Id reprehenderit voluptas ipsum incidunt aut soluta, tempora repellat fugit doloremque dolor!"}/>
+        <ServiceCard style={{
+          justifySelf: "right"
+        }} number={4} title={"Title"} paragraph={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Id reprehenderit voluptas ipsum incidunt aut soluta, tempora repellat fugit doloremque dolor!"}/>
+      </div>
+    </div>
+  );
+};
+
+export default Service;
