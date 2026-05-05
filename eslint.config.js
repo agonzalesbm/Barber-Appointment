a69faa86444e04c6c45322318799886fb4449aa5
agonzalesbm <andres.gonzales.dev@gmail.com>
2026-05-04 22:54:18 -0400

    [@agonzalesbm] eslint version change and add new rules

--- eslint.config.js
+++ eslint.config.js
@@ -3,13 +3,18 @@ import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import { defineConfig, globalIgnores } from "eslint/config";
+import react from "eslint-plugin-react";
 
 export default defineConfig([
   globalIgnores(["dist"]),
   {
     files: ["**/*.{js,jsx}"],
+    plugins: {
+      react: react
+    },
     extends: [
       js.configs.recommended,
+      react.configs.flat.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
     ],
@@ -21,7 +26,9 @@ export default defineConfig([
   {
     rules: {
       "semi": ["error", "always"],
-      "quotes": ["error", "double"]
+      "quotes": ["error", "double"],
+      "react/react-in-jsx-scope": "off",
+      "react/prop-types": "off"
     }
   }
 ]);
